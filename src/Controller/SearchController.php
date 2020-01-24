b005807906da0a248b9c23c8b1ef24de08c28df5
Michel Weimerskirch <michel@weimerskirch.net>
2020-01-24 15:51:24 +0100

    Fixed issue: autocomplete always returned default language

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -23,16 +23,22 @@ class SearchController extends ControllerBase {
    */
   public function search(Request $request) {
     $search = $request->query->get('search');
+    $currentLanguage = \Drupal::languageManager()->getCurrentLanguage()->getId();
     // [{"id":1,"title":"Hello world!","url":"http:\/\/localhost\/wordpress\/2019\/03\/04\/hello-world\/","type":"post","subtype":"post","_links":{"self":[{"embeddable":true,"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/posts\/1"}],"about":[{"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/types\/post"}],"collection":[{"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/search"}]}},{"id":2,"title":"Sample Page","url":"http:\/\/localhost\/wordpress\/sample-page\/","type":"post","subtype":"page","_links":{"self":[{"embeddable":true,"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/pages\/2"}],"about":[{"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/types\/page"}],"collection":[{"href":"http:\/\/localhost\/wordpress\/wp-json\/wp\/v2\/search"}]}}]
     $query = \Drupal::entityQuery('node');
     $query->condition('title', $search, 'CONTAINS');
     $query->condition('status', 1);
+    $query->condition('langcode', $currentLanguage);
     $query->sort('created', 'DESC');
 
     $node_ids = $query->execute();
     $nodes = Node::loadMultiple($node_ids);
     $result = [];
     foreach ($nodes as $node) {
+      // Check for translation
+      if ($node->hasTranslation($currentLanguage)) {
+        $node = $node->getTranslation($currentLanguage);
+      }
       $result[] = [
         'id' => $node->id(),
         'title' => $node->getTitle(),
